3fcbb9285f6f531d38b9ccb1c4a21cfc6d128f42
Shravan Dwarka <liizalbert_1304@live.com>
2018-03-01 23:42:17 +0400

    pushed

--- Hashcode/src/hashcode2018/Vehicle.java
+++ Hashcode/src/hashcode2018/Vehicle.java
@@ -1,3 +1,5 @@
+package hashcode2018;
+
 public class Vehicle
 {
   private Coordinate currentPosition;
@@ -8,11 +10,16 @@ public class Vehicle
 
   private int phase;
 
-  private Ride assignedRide
+  private int rideStatus;
 
-  private int PHASE_GO_TO_START_INTERSECTION = 1;
-  private int PHASE_WAITING = 2;
-  private int PHASE_GO_TO_FINAL_INTERSECTION = 3;
+  private Ride assignedRide;
+
+  private static int PHASE_GO_TO_START_INTERSECTION = 1;
+  private static int PHASE_WAITING = 2;
+  private static int PHASE_GO_TO_FINAL_INTERSECTION = 3;
+ 
+  private static int DOING_RIDE = 1;
+  private static int NOT_DOING_RIDE = 2;
 
 
   public Vehicle()
@@ -27,24 +34,46 @@ public class Vehicle
     startIntersection = ride.getStartIntersection();
     finalIntersection = ride.getFinalIntersection();
     phase = PHASE_GO_TO_START_INTERSECTION;
+    rideStatus = DOING_RIDE;
   }
 
   public void moveVehicle(int step)
   {
-    switch (phase)
-    {
-      case PHASE_GO_TO_START_INTERSECTION:
-        if (currentPosition.equals(startIntersection))
-          if (step < ride.getEarliest())
-            phase = WAITING;
-          else 
-            moveVehicleTowardsDesiredCoordinate(startIntersection);
-        break;
-      case PHASE_WAITING:
+    //Encoded as a series of ifs and not a switch so that changes from a phase to another (e.g. from start to waiting) in the same step takes effect immediately
 
-    }
+    if (phase == PHASE_GO_TO_START_INTERSECTION)
+      if (currentPosition.equals(startIntersection))
+        phase = PHASE_WAITING;
+      else
+        moveVehicleTowardsDesiredCoordinate(startIntersection);
+
+    if (phase == PHASE_WAITING)
+      if (step >= assignedRide.getEarliest())
+        phase = PHASE_GO_TO_FINAL_INTERSECTION;
+
+    if (phase == PHASE_GO_TO_FINAL_INTERSECTION)
+      if (currentPosition.equals(finalIntersection))
+        rideStatus = NOT_DOING_RIDE;
+      else
+        moveVehicleTowardsDesiredCoordinate(finalIntersection);
   }
-
-
-
+  
+  public void moveVehicleTowardsDesiredCoordinate(Coordinate st)
+  {
+	  int x = st.getX();
+	  int y = st.getY();
+	  
+	  int a = currentPosition.getX();
+	  int b = currentPosition.getY();
+	  
+	  if (x > a)
+		  a++;
+	  else if (x < a)
+		  a--;
+	  else if (y > b)
+		  b++;
+	  else if (y < b)
+		  b--;
+	  
+  }
 }
